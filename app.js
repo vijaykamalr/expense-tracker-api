c8695aec90c1b8659fa45fed3c4dd2d9379b5c1b
vijaykamalr <vijaykamalr@gmail.com>
2021-12-30 22:41:48 +0530

    added the user

--- app.js
+++ app.js
@@ -1,31 +1,29 @@
-const functions = require('firebase-functions');
-var express = require('express');
-var path = require('path');
-var cookieParser = require('cookie-parser');
-var logger = require('morgan');
+var express = require("express");
+var path = require("path");
+var cookieParser = require("cookie-parser");
+var logger = require("morgan");
 
-var cors = require('cors')
-var indexRouter = require('./routes/index');
+var cors = require("cors");
+var indexRouter = require("./routes/index");
 var corsOptions = {
-    origin: 'http://localhost:3000',
-    optionsSuccessStatus: 200 // some legacy browsers (IE11, various SmartTVs) choke on 204
-  }
-
+  origin: "*",
+  optionsSuccessStatus: 200, // some legacy browsers (IE11, various SmartTVs) choke on 204
+};
 
 var app = express();
 
-app.use(cors(corsOptions))
+app.use(cors(corsOptions));
 
-app.use(logger('dev'));
+app.use(logger("dev"));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, "public")));
 
-app.use('/expense', indexRouter);
+app.use("/expense", indexRouter);
 
 var port = process.env.PORT || 8080;
-app.listen(port,function(){
-  console.log('app listen in '+port)
-})
-exports.app = functions.https.onRequest(app);
+app.listen(port, function () {
+  console.log("app listen in " + port);
+});
+module.exports = app;
--- routes/index.js
+++ routes/index.js
@@ -5,8 +5,16 @@ const { v4: uuidv4 } = require("uuid");
 
 /* GET home page. */
 router.post("/transaction", async function (req, res, next) {
-  console.log(req.body)
-  const snapshot = await db.collection("IncomeExpense").get();
+  const body = req.body;
+  let snapshotQuery = db
+    .collection("users")
+    .doc("etdeJSTEkNIlTdFdEkaK")
+    .collection("expense");
+  if (body.startDate && body.endDate)
+    snapshotQuery = snapshotQuery
+      .where("date", ">=", body.startDate)
+      .where("date", "<=", body.endDate);
+  const snapshot = await snapshotQuery.get();
   if (snapshot.size === 0) res.json([]);
   else {
     let expenseData = [];
@@ -14,27 +22,17 @@ router.post("/transaction", async function (req, res, next) {
       expenseData.push(doc.data());
       return doc.data();
     });
-    console.log(expenseData);
     res.json(expenseData);
   }
-  //   const expenseDb = db.collection('IncomeExpense')
-  //   const getExpenses = await expenseDb.get();
-
-  // const observer = await getExpenses.forEach(snapshot => {
-  //   console.log(`changes: ${JSON.stringify(snapshot.data())}`);
-  //   return snapshot.data()
-  //  }, err => {
-  //   console.log(`Error: ${err}`);
-  //  });
-  //   console.log(observer);
 });
 
 router.post("/", async function (req, res, next) {
-  const expenseDb = db.collection("IncomeExpense");
-  console.log(req.body);
+  const expenseDb = db
+    .collection("users")
+    .doc("etdeJSTEkNIlTdFdEkaK")
+    .collection("expense");
   const addExpenseDB = expenseDb.doc(uuidv4());
   const addExpense = await addExpenseDB.set(req.body);
-  console.log(addExpense);
   res.json(addExpense);
 });
 
